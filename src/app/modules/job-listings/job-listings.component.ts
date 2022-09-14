50255a4c825db57c3c3ee632835cfffb020ebda3
Jaadec <jaap.dechering@gmail.com>
2022-09-14 12:56:50 +0200

    Rework the table into multiple cards and style it accordingly

--- src/app/modules/job-listings/job-listings.component.ts
+++ src/app/modules/job-listings/job-listings.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import {COMMA, ENTER, SPACE} from '@angular/cdk/keycodes';
+import { IFilter } from 'src/app/models/filter.model';
 import { IJob } from 'src/app/models/job.model';
 import { JobService } from 'src/app/services/job.service';
+import { MatChipInputEvent } from '@angular/material/chips';
 
 @Component({
   selector: 'app-job-listings',
@@ -9,10 +12,14 @@ import { JobService } from 'src/app/services/job.service';
   styleUrls: ['./job-listings.component.css']
 })
 export class JobListingsComponent implements OnInit {
-
-
-  availableColumns = ['logo','company', 'role', 'level'];
-  datasource = new MatTableDataSource<IJob>();
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA, SPACE];
+  filters: String[] = ['JavaScript'];
+  availableColumns = ['logo','company', 'filters'];
+  datasource: IJob[] = [];
 
   constructor (
     private jobService: JobService
@@ -20,9 +27,59 @@ export class JobListingsComponent implements OnInit {
 
   ngOnInit() {
     this.jobService.getJobs().subscribe(result => {
-      console.log(result);
-      this.datasource.data = result
+      this.datasource = result
     })
-    
+  }
+
+  getFilterAttributes(element: IJob): String[] {
+    return [element.role, element.level, ...element.languages, ...element.tools]
+  }
+
+  isFilter(element: IJob): Boolean {
+    if (this.filters.length === 0) {
+      return true;
+    }
+
+    const filterAttr = this.getFilterAttributes(element);
+
+    // Checks if all filters are in the filter attributes of element
+    return this.filters.every(fil => {
+      return filterAttr.indexOf(fil) != -1
+    });
+  }
+
+  importLogo(localPath: String) {
+    return 'assets/' + localPath.slice(1);
+  }
+
+  add(event: MatChipInputEvent): void {
+    const input = event.chipInput;
+    const value = event.value;
+
+    if ((value || '').trim()) {
+      this.filters.push(value.trim());
+    }
+
+    if (input) {
+      input.clear();
+    }
+  }
+
+  addFilterAttr(filter: String): void {
+    if ((filter || '').trim()) {
+      this.filters.push(filter.trim());
+    }
+  }
+
+  remove(filter: String): void {
+    const index = this.filters.indexOf(filter);
+
+    if (index >= 0) {
+      this.filters.splice(index, 1);
+    }
+  }
+
+  clear(): void {
+    this.filters = [];
   }
 }
